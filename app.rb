b74663bfedc18113d20fb94f930da0dd2488b776
ruhenheim <asd.uchiyama@gmail.com>
2019-02-14 21:33:47 +0900

    挨拶文の表示をhelper化して改善

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
+# p settings.server
 
 get '/' do
   erb :index
 end
 post '/' do
   erb :index
+end
+
+helpers do
+  def greeting(sentence, name, age)
+    name = "匿名" if name.empty?
+    age  = "年齢不詳" if age.empty?
+    "#{sentence}、#{name}(#{age})さん"
+  end
 end
