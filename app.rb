55f22aa8137ebc184a14b0eb70fee53c3d79fc71
ruhenheim <asd.uchiyama@gmail.com>
2019-03-25 15:37:28 +0900

    helperのバグ

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ end
 
 helpers do
   def greeting(sentence, name, age)
-    name = "匿名" if name.empty?
-    age  = "年齢不詳" if age.empty?
+    name = "匿名" if (name.nil? || name == '')
+    age  = "年齢不詳" if (age.nil? || age == '')
     "#{sentence}、#{name}(#{age})さん"
   end
 end
